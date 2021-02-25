edac98a56f564a92da33bc0cfe15600728cadceb
zhangyong <1761656766@qq.com>
2021-02-25 12:01:52 +0800

    前段项目的初始化

---
+++ src/api/user.js
@@ -0,0 +1,25 @@
+import request from '@/utils/request'
+
+// 用户的接口位置
+export function login(data) {
+  return request({
+    url: '/vue-element-admin/user/login',
+    method: 'post',
+    data
+  })
+}
+
+export function getInfo(token) {
+  return request({
+    url: '/vue-element-admin/user/info',
+    method: 'get',
+    params: { token }
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/vue-element-admin/user/logout',
+    method: 'post'
+  })
+}
---
+++ src/permission.js
@@ -0,0 +1,70 @@
+/* eslint-disable no-irregular-whitespace */
+import router from './router'
+import store from './store'
+import { Message } from 'element-ui'
+import NProgress from 'nprogress' // progress bar
+import 'nprogress/nprogress.css' // progress bar style
+import { getToken } from '@/utils/auth' // get token from cookie
+import getPageTitle from '@/utils/get-page-title'
+
+NProgress.configure({ showSpinner: false }) // NProgress Configuration
+
+const whiteList = ['/login', '/auth-redirect'] // no redirect whitelist
+
+router.beforeEach(async(to, from, next) => {
+  // start progress bar
+  NProgress.start()
+
+  // 设置页面标题
+  document.title = getPageTitle(to.meta.title)
+
+  // 从vue里面得到用户登录的token
+  const hasToken = getToken()
+
+  // 判断是否有token
+  if (hasToken) {
+    if (to.path === '/login') {
+      // 如果登录了，跳转到首页
+      next({ path: '/' })
+      NProgress.done()
+    } else {
+      // 确定用户是否已通过getInfo获得其权限角色
+      const hasRoles = store.getters.roles && store.getters.roles.length > 0
+      if (hasRoles) {
+        next()
+      } else {
+        try {
+          // 如果没有得到权限则再去请求后台得到用户信息及权限信息
+          const { roles } = await store.dispatch('user/getInfo')
+
+          // 绑定动态路由【后面我们要修改】
+          const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
+
+          // 添加动态路由到主路由
+          router.addRoutes(accessRoutes)
+
+          next({ ...to, replace: true })
+        } catch (error) {
+          // 如果出现异常，请求后台重置用户的token 并跳转到登陆页
+          await store.dispatch('user/resetToken')
+          Message.error(error || 'Has Error')
+          next(`/login?redirect=${to.path}`)
+          NProgress.done()
+        }
+      }
+    }
+  } else {
+    // 如果没有token就跳转到登陆页
+    if (whiteList.indexOf(to.path) !== -1) {
+      next()
+    } else {
+      next(`/login?redirect=${to.path}`)
+      NProgress.done()
+    }
+  }
+})
+
+router.afterEach(() => {
+  // 完成路由的加载
+  NProgress.done()
+})
